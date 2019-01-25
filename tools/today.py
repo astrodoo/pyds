7160422611a3eced8f8efa53f18e904e425aaa4c
Doosoo Yoon <astrodoo@gmail.com>
2019-01-25 13:02:38 +0100

    add today.py

--- tools/today.py
+++ tools/today.py
@@ -17,6 +17,9 @@ import datetime
 class today:
     """ 
     generate the string that indicates the today with the format of "yymmdd_"
+
+    ex) from pyds.tools import today
+        print today.datestr
     """
 
     datestr = datetime.datetime.now().strftime("%y%m%d") + '_'
